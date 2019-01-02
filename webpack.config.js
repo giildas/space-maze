e39bbd1a14a87014d9ae7e63300b6aac740aad21
gildas <giildas@gmail.com>
2019-01-02 17:41:17 +0100

    es6 boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  mode: 'development',
+  entry: __dirname + '/src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+        },
+      }
+    ]
+  }
+};
